471fdddaf7282ed26ff4a055818dce81e74be430
Astrian Zheng <mingriqd@gmail.com>
2019-04-04 23:25:35 +0800

    bug fix

--- api/checkpoc.js
+++ api/checkpoc.js
@@ -8,7 +8,7 @@ module.exports = async function (id) {
     } else if (config.ifslist[i].respoc === id) {
       faction = 'RES'
     }
-    console.log(config.ifslist[i].location})
+    console.log(config.ifslist[i].location)
     if (faction !== '') return { faction, location: config.ifslist[i].location}
   }
   throw `抱歉，你没有权限使用本 bot，请联系你所在阵营的 PoC（Res @Nickywong, Enl @wkbond）完成签到。希望你可以在 Ingress First Saturday 玩得愉快！`
