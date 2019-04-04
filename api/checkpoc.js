c41a3703a06fd7d4ced7a01af942d109bf022561
Astrian Zheng <mingriqd@gmail.com>
2019-04-04 23:27:41 +0800

    bug fix

--- api/checkpoc.js
+++ api/checkpoc.js
@@ -1,15 +1,14 @@
 module.exports = async function (id) {
-  let ifslist = JSON.parse(process.env.IFS_INFO)
-  console.log(ifslist)
-  for (let i in ifslist) {
+  let ifs = JSON.parse(process.env.IFS_INFO)
+  console.log(ifs)
+  for (let i in ifs) {
     let faction = ''
-    if (config.ifslist[i].enlpoc === id) {
+    if (config.ifs[i].enlpoc === id) {
       faction = 'ENL'
-    } else if (config.ifslist[i].respoc === id) {
+    } else if (config.ifs[i].respoc === id) {
       faction = 'RES'
     }
-    console.log(config.ifslist[i].location)
-    if (faction !== '') return { faction, location: config.ifslist[i].location}
+    if (faction !== '') return { faction, location: config.ifs[i].location}
   }
-  throw `抱歉，你没有权限使用本 bot，请联系你所在阵营的 PoC（Res @Nickywong, Enl @wkbond）完成签到。希望你可以在 Ingress First Saturday 玩得愉快！`
+  throw `抱歉，你没有权限使用本 bot，请联系你所在阵营的 PoC 完成签到。希望你可以在 Ingress First Saturday 玩得愉快！`
 }
