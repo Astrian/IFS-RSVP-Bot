c7b975aebdfdb661e89b958ce4c268ff08f8eef3
Astrian Zheng <mingriqd@gmail.com>
2019-05-17 16:35:24 +0800

    debugging

--- api/checkapstatus.js
+++ api/checkapstatus.js
@@ -22,6 +22,10 @@ function getData(base, faction, location, identity) {
       records.forEach((record) => {
         res(record.get('特工代号'))
       })
-    }, error => if (error) { err(error) })
+    }, error => {
+      if (error) {
+        err(error)
+      }
+    })
   })
 }
