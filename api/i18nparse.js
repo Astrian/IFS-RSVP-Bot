ebe7b2f7b0c3e9874dcc520b6e4f470409c6a11f
Astrian Zheng <mingriqd@gmail.com>
2019-05-17 20:57:23 +0800

    complete i18n config

--- api/i18nparse.js
+++ api/i18nparse.js
@@ -1,10 +1,10 @@
 // Import i18n file
-const i18n = require('../i18n.json')
+const i18n = require('../i18n')
 const debug = require('debug')('rsvpbot:api/i18n.js')
 
-module.exports = async function (string, obj) {
+module.exports = async function (string, obj, mode) {
   let lang = process.env.I18N || "en_us"
-  let str = i18n.botscript[lang][string] || i18n.botscript["en_us"][string]
+  let str = i18n[lang][mode ? mode : 'botscript'][string] || i18n[lang][mode ? mode : 'botscript'][string]
   Object.keys(obj).forEach(key => {
     str = str.replace(new RegExp(`{${key}}`,'g'), obj[key]);
   })
---
+++ i18n/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  zh_cn: require('./zh_cn.json'),
+  en_us: require('./en_us.json')
+}
