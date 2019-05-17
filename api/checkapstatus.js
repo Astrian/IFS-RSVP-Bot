090c6342b0ef6f7d3d2c2f6ee293418ff794c145
Astrian Zheng <mingriqd@gmail.com>
2019-05-17 16:42:28 +0800

    debugging

--- api/checkapstatus.js
+++ api/checkapstatus.js
@@ -19,13 +19,12 @@ function getData(base, faction, location, identity) {
       view: "Grid view",
       filterByFormula: `AND (NOT ({正在登记经验值} = ''), {阵营} = '${faction}', {操作人} = ${identity})`
     }).eachPage(function page(records, fetchNextPage) {
-    records.forEach(function(record) {
-        console.log('Retrieved', record.get('特工代号'));
-    });
-    res('ok')
-
-    }, function done(err) {
-        if (error) { err(error) }
+      records.forEach(function(record) {
+          console.log('Retrieved', record.get('特工代号'));
+      });
+      res('ok')
+    }, function done(error) {
+      if (error) { err(error) }
     });
   })
 }
