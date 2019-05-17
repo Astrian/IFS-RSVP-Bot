61436662b19299f3c4e4fd8bedd624cab3e8fee1
Astrian Zheng <mingriqd@gmail.com>
2019-05-17 15:39:33 +0800

    debugging

--- api/checkapstatus.js
+++ api/checkapstatus.js
@@ -8,19 +8,19 @@ const i18n = require('./i18nparse')
 // Import RSVP base
 const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.BASE_ID);
 
-module.exports = async function (faction, location, identity) {
+module.exports = async function (base, faction, location, identity) {
   // Check AP log status
   debug('fetching infomation from airtable...')
   let loggingstatus = await status
   debug(loggingstatus)
 }
 
-function status(location, faction, identity) {
+function status(base, location, faction, identity) {
   return new Promise((err, res) {
     base(location).select({
       maxRecords: 1,
       view: "Grid view",
       filterByFormula: `AND(NOT({正在登记经验值} = ''), {阵营} = '${faction}', {操作人} = ${identity})`
-    }).firstPage().then(result => res(result))
+    }).firstPage(result => res(result))
   })
 }
