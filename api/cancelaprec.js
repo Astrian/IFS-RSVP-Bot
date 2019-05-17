369f82aaac579d56d08171156357cd52a22501b6
Astrian Zheng <mingriqd@gmail.com>
2019-05-17 10:30:06 +0800

    debug

--- api/cancelaprec.js
+++ api/cancelaprec.js
@@ -1,10 +1,12 @@
 // Import Airtable API and config
 const Airtable = require('airtable');
+const debug = require('debug')('api:cancelaprec.js')
 
 // Import RSVP base
 const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.BASE_ID);
 
 module.exports = async function (faction, location, operator) {
+  debug('====== cancelaprec =====')
   // get agent record
   let agentsbase = await base(location).select({
     view: "Grid view",
