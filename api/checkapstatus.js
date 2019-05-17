85c5d1280c5641c2e741ac4ec29aadbad36442ab
Astrian Zheng <mingriqd@gmail.com>
2019-05-17 18:05:33 +0800

    add new i18n string

--- api/checkapstatus.js
+++ api/checkapstatus.js
@@ -9,7 +9,7 @@ module.exports = async function (faction, location, identity) {
   const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.BASE_ID)
   let sth = await getData(base, faction, location, identity)
   if (sth.error) {
-    throw 'error!'
+    throw i18n('datainput_error_inputstatus', {agent: sth.id})
   }
   return
 }
