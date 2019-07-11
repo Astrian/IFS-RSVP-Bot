f0456b6940bab4c2997e58da6794ba0f5cef7693
Astrian Zheng <mingriqd@gmail.com>
2019-07-11 20:31:12 +0800

    add godmode

--- api/cancelaprec.js
+++ api/cancelaprec.js
@@ -2,8 +2,14 @@
 const Airtable = require('airtable');
 const debug = require('debug')('api:cancelaprec.js')
 
+// debug switch
+const godmode = require('../godmode.json')
+let env
+if (godmode.godmode) env = godmode.config
+else env = process.env
+
 // Import RSVP base
-const base = new Airtable({apiKey: process.env.AIRTABLE_TOKEN}).base(process.env.BASE_ID);
+const base = new Airtable({apiKey: env.AIRTABLE_TOKEN}).base(env.BASE_ID);
 
 module.exports = async function (faction, location, operator) {
   debug('====== cancelaprec =====')
